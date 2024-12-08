0c5816a2d5d38cba618f02b658fb59c115b542a4
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-12-08 15:49:40 +0100

    added QuoteCard comp to Home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Header from "@/components/Header";
+import QuoteCard from '../components/QuoteCard';
 
 export default function Home() {
   return (
-    <div className="text-lg text-blue-500"><h1>Home page</h1></div>
+    <div className="container text-white">
+      <Header></Header>
+      <main className="">
+        <h1>Home Page</h1>
+        <QuoteCard quote="This is the legend of Cassius Clay" />
+      </main>
+    </div>
   );
 }
