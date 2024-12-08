a758da92cd809bc91e16a5f5f72461366ce9776a
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-12-08 17:47:50 +0100

    style changes.

--- components/QuoteCard.tsx
+++ components/QuoteCard.tsx
@@ -14,21 +14,22 @@ const QuoteCard = ({ quote }: { quote: string }) => {
   return (
     <div className="max-w-sm mx-auto border border-gray-700 rounded-lg shadow-lg overflow-hidden">
       {/* Header */}
-      <div className="bg-blue-950 text-white text-lg font-bold p-4">
+      <div className="bg-blue-950 text-white text-lg font-bold p-4 text-center">
         Quote of the Day
       </div>
 
       {/* Body */}
-      <div className="bg-white text-gray-800 py-6 text-center">
+      <div className="bg-white text-gray-800 p-6 text-center">
         <p className="text-base pb-2">{quote}</p>
+        <p className="text-right italic">-- author name</p>
         <button className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600 transition duration-200">
           New Quote
         </button>
       </div>
 
       {/* Footer */}
-      <div className="bg-blue-950 p-4">
-        <button onClick={copyToClipboard}><FontAwesomeIcon icon={faCopy} /></button>
+      <div className="bg-blue-950 p-4 text-right">
+        <button className='hover:text-gray-400 transition duration-200' onClick={copyToClipboard}><FontAwesomeIcon icon={faCopy} /></button>
       </div>
     </div>
   );
