c52f6310f8c340630482d67333474a0b56bb344a
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-12-09 11:17:02 +0100

    header bg color changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 export default function Header() {
-  return <header className="flex justify-start items-center ps-2 py-4 gap-4 shadow-md antialiased bg-gradient-to-br from-gray-900 to-gray-400">
+  return <header className="flex justify-start items-center ps-2 py-4 gap-4 shadow-md antialiased bg-blue-900">
     <span><Image src="/icon.png" width={50} height={50} alt="Quotation mark" /></span>
     <span><h1 className="text-4xl">Quotation Generator</h1></span>
   </header>
