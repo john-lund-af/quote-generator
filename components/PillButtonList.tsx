0037d43264904c06a1dd8d835cf201db2dfb18e4
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-12-10 20:17:24 +0100

    Created PillButtonList comp.

---
+++ components/PillButtonList.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useContext } from 'react';
+import QuoteContext from '@/context/QuoteContext';
+import PillButton from './PillButton';
+
+export default function PillButtonList() {
+  const { isLoading, allQuotes } = useContext(QuoteContext);
+
+  if (isLoading)
+    return <h1 className="text-2xl">Loading...</h1>
+
+  const authors = Array.from(new Set<string>(allQuotes.map(quote => quote.author)));
+  const ascAuthors = authors.toSorted();
+
+  return <ul className="flex gap-4 flex-wrap">
+    {ascAuthors.map(author => <li key={author}><PillButton author={author} /></li>)}
+  </ul>
+}
