f8affbb8568c4dbbb72e99691b1ce27fb42c729b
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-12-11 10:42:50 +0100

    postit card height fix.

--- components/PostItCart.tsx
+++ components/PostItCart.tsx
@@ -2,7 +2,7 @@ import IQuote from "@/types/IQuote";
 
 export default function PostItCard({ quote }: { quote: IQuote }) {
   return (
-    <div className="grow p-6 bg-white border border-gray-200 rounded-lg shadow w-[360px] h-[200px]">
+    <div className="grow p-6 bg-white border border-gray-200 rounded-lg shadow w-[360px] min-h-[225px]">
 
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{quote.author}</h5>
       <p className="font-normal text-gray-900">{quote.quote}</p>
